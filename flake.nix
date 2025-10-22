68759c843b19e10dcd50629a707befa7503fe7dc
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2025-10-22 18:36:52 +0200

    chore: add flake.nix for nix package manager, use bun for angular

---
+++ flake.nix
@@ -0,0 +1,75 @@
+{
+  description = "Franklyn all deps flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+
+      rust = pkgs.rust-bin.stable.latest.default.override {
+        extensions = [
+          "rust-src"
+          "rust-analyzer"
+          "clippy"
+          "rustfmt"
+        ];
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs;
+          [
+            # java
+            jdk17_headless
+            maven
+            quarkus
+
+            # angular
+            bun
+
+            # hugo
+            hugo
+            go
+
+            # rust
+            rust
+            xorg.libxcb
+
+            # Cargo tools
+            cargo
+            cargo-bloat # Analyze binary size
+            cargo-edit # Add/remove dependencies from CLI
+            cargo-outdated # Check for outdated dependencies
+            cargo-udeps # Find unused dependencies
+            cargo-watch # Auto-rebuild on file changes
+
+            # Additional useful tools
+            pkg-config
+            openssl
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.darwin.apple_sdk.frameworks.Security
+            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+          ];
+
+        shellHook = ''
+
+        '';
+
+        LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+        BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${pkgs.llvmPackages.libclang.lib}/lib/clang/${pkgs.llvmPackages.clang.version}/include";
+      };
+    });
+}
