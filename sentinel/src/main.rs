89538c7900329a869c06b8020f3b85eb59df99c9
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2025-11-12 15:21:22 +0100

    chore: add rust linting and formatting

--- sentinel/src/main.rs
+++ sentinel/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
+    println!("{}", hello_world());
+
+    let helloVar = "deimmo";
+
     println!("Hello, world!");
+}
+
+const fn hello_world() -> &'static str {
+    "hello world"
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_hello_world() {
+        println!("{}", hello_world());
+        assert_eq!(hello_world(), "hello world");
+    }
+
+    #[test]
+    fn test_hello_world2() {
+        assert_eq!(hello_world(), "hello world");
+    }
 }
