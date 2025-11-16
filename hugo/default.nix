75607c789ff4dec5d185b4d09afd46bc667ebc84
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2025-11-16 23:14:47 +0100

    feat(docs): add github issues list for gh repo

--- hugo/default.nix
+++ hugo/default.nix
@@ -15,6 +15,12 @@
           hugo --gc --minify "$@"
         '')
       ];
+
+      shellHook = ''
+        # !!! this should be the only point where the github url is referenced on the entire website
+        # !!! changing this to the new repo should make everything work again
+        export HUGO_GITHUB_PROJECT_URL="https://github.com/2526-4ahitm-itp/2526-4ahitm-franklyn"
+      '';
     };
   };
 }
