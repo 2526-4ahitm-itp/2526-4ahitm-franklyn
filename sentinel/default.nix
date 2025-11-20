4d10e38fa541890985c8bd5298ad6c35c5532540
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2025-11-20 23:14:23 +0100

    chore: add nix package for franklyn sentinel

--- sentinel/default.nix
+++ sentinel/default.nix
@@ -5,61 +5,63 @@
     project-version,
     ...
   }: let
-    linuxBuildInputs = with pkgs; [
-      xorg.libxcb
-      xorg.libXrandr
-      dbus
-      pipewire
-      wayland
-      libGL
-      libgbm
+    scripts = [
+      (pkgs.writeScriptBin "fr-sentinel-pr-check" ''
+        set +e
+        failed=0
+
+        cargo fmt --check || failed=1
+
+        cargo clippy --all-targets --all-features \
+          --message-format=short || failed=1
+
+        cargo test || failed=1
+
+        exit $failed
+      '')
+
+      (pkgs.writeScriptBin "fr-sentinel-build" ''
+        set -e
+
+        cargo build --release
+      '')
     ];
 
-    nativeBuildInputs = with pkgs;
-      [
-        rust-bin.stable.latest.default
+    commonBuildInputs = with pkgs; [
+      rust-bin.stable.latest.default
 
-        pkg-config
-        llvmPackages.libclang.lib
-        clang
+      pkg-config
+      llvmPackages.libclang.lib
+      udev
+      clang
+    ];
 
-        ffmpeg-full
+    platformBuildInputs =
+      [pkgs.ffmpeg]
+      ++ pkgs.lib.optionals pkgs.stdenv.isLinux
+      (with pkgs; [
+        xorg.libxcb
+        xorg.libXrandr
+        dbus
+        pipewire
+        wayland
+        wayland-protocols
+        libGL
+        libgbm
+      ]);
 
-        # Cargo tools
-        cargo
-        cargo-bloat # Analyze binary size
-        cargo-edit # Add/remove dependencies from CLI
-        cargo-outdated # Check for outdated dependencies
-        cargo-udeps # Find unused dependencies
-        cargo-watch # Auto-rebuild on file changes
+    commonDevInputs = with pkgs; [
+      cargo
+      cargo-bloat # Analyze binary size
+      cargo-edit # Add/remove dependencies from CLI
+      cargo-outdated # Check for outdated dependencies
+      cargo-udeps # Find unused dependencies
+      cargo-watch # Auto-rebuild on file changes
+    ];
 
-        reviewdog
-
-        (pkgs.writeScriptBin "fr-sentinel-pr-check" ''
-          set +e
-          failed=0
-
-          cargo fmt --check || failed=1
-
-          cargo clippy --all-targets --all-features \
-            --message-format=short || failed=1
-
-          cargo test || failed=1
-
-          exit $failed
-        '')
-
-        (pkgs.writeScriptBin "fr-sentinel-build" ''
-          set -e
-
-          cargo build --release
-        '')
-      ]
-      ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-        pkgs.darwin.apple_sdk.frameworks.Security
-        pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-      ]
-      ++ pkgs.lib.optionals pkgs.stdenv.isLinux linuxBuildInputs;
+    platformDevInputs =
+      pkgs.lib.optionals pkgs.stdenv.isDarwin []
+      ++ pkgs.lib.optionals pkgs.stdenv.isLinux [];
   in {
     _module.args.pkgs = import inputs.nixpkgs {
       inherit system;
@@ -69,14 +71,9 @@
     };
 
     devShells.sentinel = pkgs.mkShell {
-      inherit nativeBuildInputs;
+      nativeBuildInputs = commonBuildInputs ++ platformBuildInputs ++ commonDevInputs ++ platformBuildInputs;
 
-      LIBCLANG_PATH = "${pkgs.llvmPackages_16.libclang.lib}/lib";
-      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (
-        if pkgs.stdenv.isLinux
-        then linuxBuildInputs
-        else []
-      );
+      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath platformBuildInputs;
 
       shellHook = ''
         export LIBCLANG_PATH="${pkgs.llvmPackages_16.libclang.lib}/lib"
@@ -86,8 +83,26 @@
     packages.franklyn-sentinel = pkgs.rustPlatform.buildRustPackage rec {
       name = "franklyn-sentinel";
       version = project-version;
-      cargoLock.lockFile = ./Cargo.lock;
       src = pkgs.lib.cleanSource ./.;
+
+      cargoLock = {
+        lockFile = ./Cargo.lock;
+        allowBuiltinFetchGit = true;
+      };
+
+      buildType = "release";
+
+      nativeBuildInputs = commonBuildInputs;
+
+      buildInputs = platformBuildInputs;
+
+      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath platformBuildInputs;
+      LIBCLANG_PATH = "${pkgs.llvmPackages_16.libclang.lib}/lib";
+
+      meta = {
+        homepage = "https://2526-4ahitm-itp.github.io/2526-4ahitm-franklyn/";
+        license = pkgs.lib.licenses.mit;
+      };
     };
   };
 }
