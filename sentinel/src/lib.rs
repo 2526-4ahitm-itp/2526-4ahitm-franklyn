f86182bc71a0d6d01479851b1a0cd7d5fac6eacc
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2025-11-21 01:26:59 +0100

    chore: update rust build

--- sentinel/src/lib.rs
+++ sentinel/src/lib.rs
@@ -1,3 +1,12 @@
 //! TODO: add
+#![deny(clippy::unwrap_used)]
+#![deny(clippy::expect_used)]
+#![deny(clippy::match_same_arms)]
+#![deny(clippy::match_wild_err_arm)]
+#![deny(clippy::wildcard_dependencies)]
+#![deny(clippy::explicit_into_iter_loop)]
+#![deny(clippy::ignored_unit_patterns)]
+#![deny(clippy::linkedlist)]
+#![warn(clippy::missing_panics_doc)]
 
 pub mod util;
