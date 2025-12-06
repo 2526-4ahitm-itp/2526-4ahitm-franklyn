f7bc987a9bbc72245601a3375bebe31401fdcbb3
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2025-12-06 01:44:35 +0100

    chore(proctor): add env for prod and dev, add app version as variable

--- proctor/vite.config.ts
+++ proctor/vite.config.ts
@@ -3,9 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { readFileSync } from 'fs'
+
+const version = readFileSync("../VERSION", "utf-8").trim()
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    __APP_VERSION__: JSON.stringify(version)
+  },
   plugins: [
     vue(),
     vueDevTools(),
