1dbe0e24ccf439e0f4df1b2e576d4f7381dff78f
ClemiZ <c.zangenfeind@htblaleonding.onmicrosoft.com>
2026-02-08 16:49:46 +0100

    feat: changed Frame to be a record

--- server/src/main/java/at/ac/htlleonding/franklynserver/Frame.java
+++ server/src/main/java/at/ac/htlleonding/franklynserver/Frame.java
@@ -2,22 +2,5 @@ package at.ac.htlleonding.franklynserver;
 
 import java.util.UUID;
 
-public class Frame {
-    public UUID sentinelId;
-
-    public UUID frameId;
-
-    public int index;
-
-    public String data;
-
-    public Frame() {
-    }
-
-    public Frame(UUID sentinelId, UUID frameId, int index, String data) {
-        this.sentinelId = sentinelId;
-        this.frameId = frameId;
-        this.index = index;
-        this.data = data;
-    }
+public record Frame (UUID sentinelId, UUID frameId, int index, String data) {
 }
