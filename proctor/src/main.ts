7f406732b6bf75f736daf43fd55337b641c39681
ClemiZ <c.zangenfeind@htblaleonding.onmicrosoft.com>
2026-02-26 21:53:28 +0100

    feat: set up base for apollo

--- proctor/src/main.ts
+++ proctor/src/main.ts
@@ -1,13 +1,21 @@
-import { createApp } from 'vue'
+import {createApp, h, provide} from 'vue'
 import { createPinia } from 'pinia'
+import {DefaultApolloClient} from '@vue/apollo-composable'
 
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
 import '@/assets/main.css'
 
 import App from './App.vue'
 import router from './router'
+import {apolloClient} from "@/stores/ApolloClient.ts";
 
-const app = createApp(App)
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient)
+  },
+
+  render: () => h(App),
+})
 
 app.use(createPinia())
 app.use(router)
