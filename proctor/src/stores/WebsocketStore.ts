16db6b8fdba52b9622215fe654dbd67b7ae22189
GregGeig <g.geigenberger@htblaleonding.onmicrosoft.com>
2026-02-27 00:15:36 +0100

    feat: very basic functionality

--- proctor/src/stores/WebsocketStore.ts
+++ proctor/src/stores/WebsocketStore.ts
@@ -1,8 +1,8 @@
 import { defineStore } from 'pinia'
 import type { ProctorMessage, ServerMessage } from '@/types/WebsocketPayloads.ts'
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 
-export const useWebsocketStore = defineStore("websocketStore", () => {
+export const useWebsocketStore = defineStore('websocketStore', () => {
 
   const sentinelList = ref<string[]>([
     "<dummy>",
@@ -10,96 +10,96 @@ export const useWebsocketStore = defineStore("websocketStore", () => {
 
   const socket = new WebSocket("/api/ws/proctor")
 
-  const subscribedSentinel = ref<null | string>(null);
+  const frameContent = reactive<string[]>([])
 
-  const frameContent = ref<null | string>(null);
-
-  let pageCount = 0;
-  let pageAmount;
-  let sentinelsToDisplayLast;
-  let sentinelsToDisplayFirst;
+  let selectedSentinelList: string[] = []
+  let pageCount = 1
+  let sentinelsToDisplayLast: number
+  let sentinelsToDisplayFirst: number
 
   const proctorRegister: ProctorMessage = {
-    type: "proctor.register",
-    timestamp: Date.now()
+    type: 'proctor.register',
+    timestamp: Date.now(),
   }
 
-  socket.addEventListener("open", () => {
+  socket.addEventListener('open', () => {
     socket.send(JSON.stringify(proctorRegister))
   })
 
-  socket.addEventListener("message", (event) => {
+  socket.addEventListener('message', (event) => {
     const requestResult: ServerMessage = JSON.parse(event.data)
-    if (requestResult.type === "server.update-sentinels") {
-      console.log(requestResult.payload.sentinels)
-      sentinelList.value = requestResult.payload.sentinels;
-      pageAmount = sentinelList.value.length / 6;
-      sentinelsToDisplayLast = (pageCount + 1) * 6 - 1;
-      sentinelsToDisplayFirst = pageCount * 6;
-      for (const sentinel of sentinelList.value) {
-        subscribeToSentinel(sentinel)
-      }
-    } else if (requestResult.type === "server.frame") {
+    if (requestResult.type === 'server.update-sentinels') {
+      sentinelList.value = requestResult.payload.sentinels
+      subscribeToWanted()
+    } else if (requestResult.type === 'server.frame') {
       // get frames in payload
       if (requestResult.payload.frames[0]) {
-        console.log("OK")
-        frameContent.value = requestResult.payload.frames[0].data
+        console.log('OK')
+        // frameContent[] = requestResult.payload.frames[0].data
       } else {
-        console.log("NOT OK")
+        console.log('NOT OK')
       }
     }
-  });
+  })
 
   function sendMessage(message: ProctorMessage) {
     socket.send(JSON.stringify(message))
   }
 
-  function subscribeToSentinel(sentinelId: string) {
-    console.log("Subscribing to sentinel", sentinelId)
+  function subscribeToWanted() {
+    sentinelsToDisplayLast = pageCount * 6 - 1
+    sentinelsToDisplayFirst = (pageCount - 1) * 6
 
-    if (subscribedSentinel.value !== null) {
+    for (const sentinel of sentinelList.value) {
       sendMessage({
-        type: "proctor.revoke-subscription",
+        type: 'proctor.revoke-subscription',
         payload: {
-          sentinelId: subscribedSentinel.value
+          sentinelId: sentinel,
         },
-        timestamp: Date.now()
+        timestamp: Date.now(),
       })
     }
 
-    sendMessage({
-      type: "proctor.subscribe",
-      payload: {
-        sentinelId: sentinelId
-      },
-      timestamp: Date.now()
-    })
+    selectedSentinelList = [];
 
-    subscribedSentinel.value = sentinelId;
+    for (let i = 0; i < sentinelList.value.length; i++) {
+      if (i >= sentinelsToDisplayFirst && i <= sentinelsToDisplayLast) {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        selectedSentinelList.push(sentinelList.value[i]!)
+        console.log('fuck' + selectedSentinelList.length + selectedSentinelList.toString())
+        sendMessage({
+          type: 'proctor.subscribe',
+          payload: {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            sentinelId: sentinelList.value[i]!,
+          },
+          timestamp: Date.now(),
+        })
+      }
+    }
   }
 
-  function increasePageCount(){
-    if (pageCount < Math.ceil(sentinelList.value.length / 6)){
+  function increasePageCount() {
+    if (pageCount < Math.ceil(sentinelList.value.length / 6)) {
       pageCount++
       console.log('Hallo' + pageCount)
     }
+    subscribeToWanted()
   }
 
-  function decreasePageCount(){
-    if (pageCount > 1){
+  function decreasePageCount() {
+    if (pageCount > 1) {
       pageCount--
       console.log('Hallo' + pageCount)
     }
+    subscribeToWanted()
   }
 
-
   return {
-    sentinelList,
-    subscribedSentinel,
+    selectedSentinelList,
     frameContent,
     pageCount,
     decreasePageCount,
     increasePageCount,
   }
-
 })
