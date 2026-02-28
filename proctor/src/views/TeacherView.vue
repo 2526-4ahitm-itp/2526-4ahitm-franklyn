3444f67bcc4f67554bc5e8aa2ad341cf8e1db36b
ClemiZ <c.zangenfeind@htblaleonding.onmicrosoft.com>
2026-02-28 12:53:22 +0100

    feat: added TeacherView, not yet working

---
+++ proctor/src/views/TeacherView.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { useQuery } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+
+export default {
+  setup () {
+    const { result } = useQuery(gql`
+      query getTeachers {
+        teachers {
+          id
+          name
+        }
+      }
+    `)
+    return {
+      result
+    }
+  },
+}
+</script>
+
