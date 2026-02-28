25c71cb957714a72d96cc45321672548408a649c
ClemiZ <c.zangenfeind@htblaleonding.onmicrosoft.com>
2026-02-28 18:18:33 +0100

    feat: fixed TeacherView and added to routing

--- proctor/src/views/TeacherView.vue
+++ proctor/src/views/TeacherView.vue
@@ -1,21 +1,21 @@
-<script lang="ts">
+<script setup lang="ts">
 import { useQuery } from '@vue/apollo-composable'
 import gql from 'graphql-tag'
 
-export default {
-  setup () {
-    const { result } = useQuery(gql`
-      query getTeachers {
-        teachers {
-          id
-          name
-        }
-      }
-    `)
-    return {
-      result
+const { result } = useQuery(gql`
+  query getTeachers {
+    teachers {
+      id
+      name
     }
-  },
-}
+  }
+`)
 </script>
 
+<template>
+  <ul>
+    <li v-for="teacher of result.teachers" :key="teacher.id">
+      {{ teacher.name }} {{ teacher.id }}
+    </li>
+  </ul>
+</template>
