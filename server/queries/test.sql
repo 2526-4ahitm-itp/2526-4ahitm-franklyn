84be920d056f138fdea89c5953481a1f7fca9af9
ClemiZ <c.zangenfeind@htblaleonding.onmicrosoft.com>
2026-03-03 10:10:08 +0100

    feat: added update / delete to test.sql

--- server/queries/test.sql
+++ server/queries/test.sql
@@ -8,3 +8,7 @@ values ($1, $2, $3, $4, $5, $6);
 -- name: findTestById :one
 select * from fr_test
 where id = $1;
+
+update fr_test set title = $3, test_account_prefix = $4, end_time = $5, start_time = $6 WHERE id = $1;
+
+delete from fr_test WHERE id = $1;
