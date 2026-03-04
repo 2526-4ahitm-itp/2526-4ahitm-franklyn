e2fe050fdf02101eaad92e1067e8c27394ca6a0d
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2026-03-04 11:28:04 +0100

    feat: init xcode project

---
+++ ios/Mobile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mobile
+//
+//  Created by Jakob Huemer on 04/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/Mobile/MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileApp.swift
+//  Mobile
+//
+//  Created by Jakob Huemer on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
