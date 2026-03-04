8da565eeb37b408c8238f71b764c968fd3266e00
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2026-03-04 17:32:20 +0100

    feat: add graphql fr- scripts to fetch and add first query
    Using fr-ios-fetch-schema, the schema is fetched from the live server.
    To generate the code from the fetched schema run fr-ios-codegen.

---
+++ ios/Mobile/GraphQL/Generated/Schema/Objects/FindAllTestsRow.graphql.swift
@@ -0,0 +1,12 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+import ApolloAPI
+
+extension FranklynAPI.Objects {
+  static let FindAllTestsRow = ApolloAPI.Object(
+    typename: "FindAllTestsRow",
+    implementedInterfaces: [],
+    keyFields: nil
+  )
+}
