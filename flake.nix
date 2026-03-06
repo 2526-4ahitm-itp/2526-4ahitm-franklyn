b4d59d54db12db8206ad273ff69ebd3566b5d164
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2026-03-06 23:33:40 +0100

    chore: add bruno package, format ios nix file, add nixpkgs-unstable for dev deps

--- flake.nix
+++ flake.nix
@@ -3,22 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    # for dev dependencies
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs =
-    { flake-parts, ... }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } (
-      top@{
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} (
+      top @ {
         config,
         withSystem,
         moduleWithSystem,
         lib,
         pkgs,
         ...
-      }:
-      {
+      }: {
         imports = [
           ./hugo
           ./sentinel
@@ -33,61 +33,64 @@
           "aarch64-darwin"
           "aarch64-linux"
         ];
-        perSystem =
-          {
-            config,
-            system,
-            pkgs,
-            self',
-            ...
-          }:
-          let
-            # globals
-            project-version = lib.strings.removeSuffix "\n" (builtins.readFile ./VERSION);
-            project-license-text = builtins.readFile ./LICENSE;
+        perSystem = {
+          config,
+          system,
+          pkgs,
+          pkgs-unstable,
+          self',
+          ...
+        }: let
+          # globals
+          project-version = lib.strings.removeSuffix "\n" (builtins.readFile ./VERSION);
+          project-license-text = builtins.readFile ./LICENSE;
 
-            maintainers.jakob = {
-              name = "Jakob Huemer-Fistelberger";
-              email = "j.huemer-fistelberger@htblaleonding.onmicrosoft.com";
-              github = "JakobHuemer";
-            };
+          maintainers.jakob = {
+            name = "Jakob Huemer-Fistelberger";
+            email = "j.huemer-fistelberger@htblaleonding.onmicrosoft.com";
+            github = "JakobHuemer";
+          };
 
-            package-meta = {
-              homepage = "https://2526-4ahitm-itp.github.io/2526-4ahitm-franklyn/";
-              license = pkgs.lib.licenses.mit;
-            };
-          in
-          {
-            _module.args = {
-              inherit
-                project-version
-                project-license-text
-                package-meta
-                maintainers
-                ;
+          package-meta = {
+            homepage = "https://2526-4ahitm-itp.github.io/2526-4ahitm-franklyn/";
+            license = pkgs.lib.licenses.mit;
+          };
+        in {
+          _module.args = {
+            inherit
+              project-version
+              project-license-text
+              package-meta
+              maintainers
+              ;
 
-              pkgs = import inputs.nixpkgs {
-                inherit system;
-                overlays = [
-                  inputs.rust-overlay.overlays.default
-                ];
-              };
-
-              mkEnvHook =
-                envList: pkgs.lib.concatStringsSep "\n" (map (env: "export ${env.name}=${env.value}") envList);
-            };
-
-            devShells.ci = pkgs.mkShell {
-              packages = with pkgs; [
-                gh
-                jq
-                semver-tool
-                tokei
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [
+                inputs.rust-overlay.overlays.default
               ];
             };
 
-            devShells.default = pkgs.mkShell {
-              inputsFrom = [
+            pkgs-unstable = import inputs.nixpkgs-unstable {
+              inherit system;
+            };
+
+            mkEnvHook = envList: pkgs.lib.concatStringsSep "\n" (map (env: "export ${env.name}=${env.value}") envList);
+          };
+
+          devShells.ci = pkgs.mkShell {
+            packages = [
+              pkgs.gh
+              pkgs.jq
+              pkgs.semver-tool
+              pkgs.tokei
+              pkgs-unstable.bruno
+            ];
+          };
+
+          devShells.default = pkgs.mkShell {
+            inputsFrom =
+              [
                 self'.devShells.sentinel
                 self'.devShells.server
                 self'.devShells.hugo
@@ -95,8 +98,8 @@
                 self'.devShells.ci
               ]
               ++ pkgs.lib.optional pkgs.stdenv.isDarwin self'.devShells.ios;
-            };
           };
+        };
       }
     );
 }
