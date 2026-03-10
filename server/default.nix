3d5b246360b10a2ea1d188feac84891fc85d241f
EldinBegano <maskettwitch@gmail.com>
2026-03-10 22:23:12 +0100

    build: change nix hash

--- server/default.nix
+++ server/default.nix
@@ -46,7 +46,7 @@
         then pkgs.lib.fakeHash
         else if pkgs.stdenv.isDarwin
         then "sha256-uuS2+A53CE/KTHUI0u1uFh8fI26o0MNLb0Z3iy2NYio=" # darwin
-        else "sha256-DEK9xlNEdCaJ1/M1G48Iw3vAX5UsYL5VnPY673IpRzQ="; # linux
+        else "sha256-Cj9Y7QhBaW0wQOAGrQgfxrUTt0iVwvRgL+4kws+ezmU="; # linux
 
       installPhase = ''
         mkdir -p $out/lib
