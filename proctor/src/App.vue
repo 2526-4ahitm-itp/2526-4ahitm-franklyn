2668cd669664924b26016f389e76135591ce5f1d
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2026-03-14 20:05:38 +0100

    feat(proctor): add nice nav bar

--- proctor/src/App.vue
+++ proctor/src/App.vue
@@ -1,6 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavComponent from './components/NavComponent.vue'
+</script>
 
 <template>
+  <NavComponent />
   <RouterView />
 </template>
 
