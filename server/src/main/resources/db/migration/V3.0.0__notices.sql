494bf63b7b530f0cfe27598c628cdb5a04f19807
GregGeig <g.geigenberger@htblaleonding.onmicrosoft.com>
2026-03-16 16:16:45 +0100

    fix(server): use singular form of notice instead of notices in migration

--- server/src/main/resources/db/migration/V3.0.0__notices.sql
+++ server/src/main/resources/db/migration/V3.0.0__notices.sql
@@ -1,6 +1,6 @@
 create type fr_notice_type as enum ('alert', 'timed', 'single');
 
-create table fr_notices (
+create table fr_notice (
     id uuid,
     type fr_notice_type,
     content varchar(1024) not null,
