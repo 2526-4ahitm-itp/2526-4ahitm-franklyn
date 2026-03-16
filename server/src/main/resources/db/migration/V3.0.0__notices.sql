0aa1f100e94462c0f6f1d64150bddb35b7887ee0
GregGeig <g.geigenberger@htblaleonding.onmicrosoft.com>
2026-03-16 16:13:32 +0100

    fix(server): add start_time and end_time to notice banners migration

--- server/src/main/resources/db/migration/V3.0.0__notices.sql
+++ server/src/main/resources/db/migration/V3.0.0__notices.sql
@@ -4,6 +4,8 @@ create table fr_notices (
     id uuid,
     type fr_notice_type,
     content varchar(1024) not null,
+    start_time timestamp(6),
+    end_time timestamp(6),
 
     primary key (`id`)
 )
