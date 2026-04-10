db00e97be7c8f1f53cf817fa414352ad749c1d47
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2026-04-10 19:12:54 +0200

    feat(sentinel): include rust build in the build.sh

--- sentinel/packaging/windows/build.sh
+++ sentinel/packaging/windows/build.sh
@@ -1,11 +1,49 @@
 #!/usr/bin/env bash
+# CI script for running in MSYS2 UCRT64
+
 set -eu
 
+RUST_VERSION="1.91.1"
+CARGO_CMD="cargo build --release --features=prod"
+VERSION="$(cat VERSION)"
+
+echo "Installing MSYS2 dependencies..."
 pacman -S --needed --noconfirm \
     mingw-w64-ucrt-x86_64-gstreamer \
     mingw-w64-ucrt-x86_64-gst-plugins-base \
     mingw-w64-ucrt-x86_64-gst-plugins-good \
-    mingw-w64-ucrt-x86_64-gst-plugins-bad
+    mingw-w64-ucrt-x86_64-gst-plugins-bad \
+    mingw-w64-ucrt-x86_64-pkgconf \
+    mingw-w64-ucrt-x86_64-gcc \
+    mingw-w64-ucrt-x86_64-openssl \
+    zip
+
+echo "Installing Rust..."
+if ! command -v rustc &> /dev/null; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain ${RUST_VERSION}-x86_64-pc-windows-gnu --profile minimal
+else
+    rustup toolchain install ${RUST_VERSION}-x86_64-pc-windows-gnu
+    rustup default ${RUST_VERSION}-x86_64-pc-windows-gnu
+fi
+
+export PATH="/ucrt64/bin:$USERPROFILE/.cargo/bin:$PATH"
+
+export CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER="gcc"
+export CC="gcc"
+export CXX="g++"
+export AR="ar"
+export PKG_CONFIG_ALLOW_CROSS=1
+
+echo "Setting environment variables..."
+export PKG_CONFIG_PATH="/ucrt64/lib/pkgconfig"
+export OPENSSL_DIR="/ucrt64"
+export OPENSSL_LIB_DIR="/ucrt64/lib"
+export OPENSSL_INCLUDE_DIR="/ucrt64/include"
+
+echo "Building sentinel..."
+pushd sentinel
+$CARGO_CMD
+popd
 
 OUT="dist"
 
@@ -18,7 +56,7 @@ mkdir -p "$OUT/plugins"
 cp ./sentinel/target/release/franklyn-sentinel.exe "$OUT/franklyn.exe"
 
 echo "Resolving core dependencies..."
-ldd "$OUT/franklyn.exe" | grep -i -o '/ucrt64/bin/.*\.dll' | xargs -I {} cp -n "{}" "$OUT/"
+ldd "$OUT/franklyn.exe" | grep -i -o '/ucrt64/bin/.*\.dll' | xargs -I {} cp -n "{}" "$OUT/" || true
 
 echo "Copying plugins..."
 cp /ucrt64/lib/gstreamer-1.0/libgstcoreelements.dll "$OUT/plugins/"
@@ -30,9 +68,11 @@ cp /ucrt64/lib/gstreamer-1.0/libgstd3d11.dll "$OUT/plugins/"
 
 echo "Resolving plugin dependencies..."
 for plugin in "$OUT/plugins/"*.dll; do
-    ldd "$plugin" | grep -i -o '/ucrt64/bin/.*\.dll' | xargs -I {} cp -n "{}" "$OUT/" 2>/dev/null
+    ldd "$plugin" | grep -i -o '/ucrt64/bin/.*\.dll' | xargs -I {} cp -n "{}" "$OUT/" 2>/dev/null || true
 done
 
 cp ./LICENSE "$OUT/LICENSE"
 
 echo "Deployment packaged in $OUT/"
+
+zip -r franklyn-sentinel-$VERSION-x86_64-windows.zip dist/*
