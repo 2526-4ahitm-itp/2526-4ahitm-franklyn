770a7a24efbb46d2cc41c8b9867fe222f3cc3bb1
ClemiZ <c.zangenfeind@htblaleonding.onmicrosoft.com>
2026-04-25 13:10:01 +0200

    feat(server): implement updating of user settings, actual update missing though

--- server/src/main/java/at/ac/htlleonding/franklynserver/resource/setting/SettingResource.java
+++ server/src/main/java/at/ac/htlleonding/franklynserver/resource/setting/SettingResource.java
@@ -1,4 +1,34 @@
 package at.ac.htlleonding.franklynserver.resource.setting;
 
+import at.ac.htlleonding.franklynserver.oidc.OidcUserService;
+import at.ac.htlleonding.franklynserver.repository.user.UserDao;
+import at.ac.htlleonding.franklynserver.repository.user.model.Teacher;
+import at.ac.htlleonding.franklynserver.repository.user.model.User;
+import at.ac.htlleonding.franklynserver.resource.error.GraphQLBusinessException;
+import at.ac.htlleonding.franklynserver.resource.error.UserTypeMismatchException;
+import at.ac.htlleonding.franklynserver.resource.setting.model.UpdateUserSettings;
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import org.eclipse.microprofile.graphql.NonNull;
+import org.jdbi.v3.core.statement.Update;
+
 public class SettingResource {
+
+    @Inject
+    OidcUserService userService;
+
+    @Inject
+    UserDao userDao;
+
+    public @NonNull UpdateUserSettings updateSettings(@Valid @NonNull UpdateUserSettings settingsInput) throws GraphQLBusinessException {
+        Teacher t = userService.resolveJwtUser(Teacher.class);
+
+        if (t.getClass() != Teacher.class) {
+            throw new UserTypeMismatchException(Teacher.class, t.getClass(), t.id);
+        }
+
+        // TODO: return userDao.updateSettings(settingsInput, t.id) or whatever when implemented in database
+
+        return null;
+    }
 }
