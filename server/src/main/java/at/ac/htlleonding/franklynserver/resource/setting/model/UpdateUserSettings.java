c292bac88e925e3e5fa666fe8e9ca16bf2bf6c40
ClemiZ <c.zangenfeind@htblaleonding.onmicrosoft.com>
2026-04-25 13:10:01 +0200

    feat(server): implement updating of user settings, actual update missing though

--- server/src/main/java/at/ac/htlleonding/franklynserver/resource/setting/model/UpdateUserSettings.java
+++ server/src/main/java/at/ac/htlleonding/franklynserver/resource/setting/model/UpdateUserSettings.java
@@ -3,7 +3,7 @@ package at.ac.htlleonding.franklynserver.resource.setting.model;
 import jakarta.validation.constraints.NotNull;
 import org.eclipse.microprofile.graphql.NonNull;
 
-public record UserSettingsUpdate (
+public record UpdateUserSettings(
         @NonNull @NotNull String language,
         @NonNull @NotNull String theme
 ) {
