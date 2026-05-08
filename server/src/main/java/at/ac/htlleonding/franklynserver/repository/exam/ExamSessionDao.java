6ecbf42f33a2341ca7bce501cf698198d14bb7bc
EldinBegano <maskettwitch@gmail.com>
2026-05-08 16:54:11 +0200

    feat: create dao for table

---
+++ server/src/main/java/at/ac/htlleonding/franklynserver/repository/exam/ExamSessionDao.java
@@ -0,0 +1,29 @@
+package at.ac.htlleonding.franklynserver.repository.exam;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.jdbi.v3.sqlobject.config.RegisterConstructorMapper;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+
+import at.ac.htlleonding.franklynserver.repository.exam.model.ExamSession;
+import at.ac.htlleonding.franklynserver.repository.exam.model.StudentSentinel;
+
+@RegisterConstructorMapper(ExamSession.class)
+public interface ExamSessionDao {
+
+    @SqlQuery("""
+            select student_id, sentinel_id, exam_id, video_file_path
+            from fr_exam_sessions
+            where exam_id = :examId
+            """)
+    List<ExamSession> findByExamId(UUID examId);
+
+    @RegisterConstructorMapper(StudentSentinel.class)
+    @SqlQuery("""
+            select student_id, sentinel_id
+            from fr_exam_sessions
+            where exam_id = :examId
+            """)
+    List<StudentSentinel> findStudentSentinelsByExam(UUID examId);
+}
