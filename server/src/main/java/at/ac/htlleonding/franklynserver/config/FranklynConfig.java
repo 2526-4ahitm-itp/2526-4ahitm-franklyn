79221085b642890fa484aaa6de53f4f089f64e9f
EldinBegano <maskettwitch@gmail.com>
2026-05-15 21:31:41 +0200

    feat: add video path

--- server/src/main/java/at/ac/htlleonding/franklynserver/config/FranklynConfig.java
+++ server/src/main/java/at/ac/htlleonding/franklynserver/config/FranklynConfig.java
@@ -8,11 +8,18 @@ public interface FranklynConfig {
 
     Pin pin();
 
+    Video video();
+
     interface Pin {
         @WithDefault("1337")
         int min();
 
         @WithDefault("4200")
         int max();
+    }
+
+    interface Video {
+        @WithDefault("/tmp/franklyn-videos")
+        String storageDir();
     }
 }
