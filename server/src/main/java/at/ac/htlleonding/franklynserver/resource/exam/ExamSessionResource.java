76c2cb1a9c49929b158f38cd78eed001a67ceb7d
EldinBegano <maskettwitch@gmail.com>
2026-05-15 17:13:37 +0200

    feat: find students by id and role

--- server/src/main/java/at/ac/htlleonding/franklynserver/resource/exam/ExamSessionResource.java
+++ server/src/main/java/at/ac/htlleonding/franklynserver/resource/exam/ExamSessionResource.java
@@ -1,14 +1,19 @@
 package at.ac.htlleonding.franklynserver.resource.exam;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.NonNull;
 import org.eclipse.microprofile.graphql.Query;
+import org.eclipse.microprofile.graphql.Source;
 
 import at.ac.htlleonding.franklynserver.repository.exam.ExamSessionDao;
 import at.ac.htlleonding.franklynserver.repository.exam.model.ExamSession;
+import at.ac.htlleonding.franklynserver.repository.user.UserDao;
+import at.ac.htlleonding.franklynserver.repository.user.model.User;
+import at.ac.htlleonding.franklynserver.repository.user.model.UserRole;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -21,8 +26,15 @@ public class ExamSessionResource {
     @Inject
     ExamSessionDao examSessionDao;
 
+    @Inject
+    UserDao userDao;
+
     @Query
     public @NonNull List<ExamSession> allStudents(@NonNull UUID examId) {
         return examSessionDao.findByExamId(examId);
+    }
+
+    public Optional<User> student(@Source ExamSession session) {
+        return userDao.findByIdAndType(session.studentId(), UserRole.STUDENT);
     }
 }
