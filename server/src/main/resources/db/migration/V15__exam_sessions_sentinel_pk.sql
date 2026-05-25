8a5a3c17eb635624939248e30cdb27785b60b582
EldinBegano <maskettwitch@gmail.com>
2026-05-25 13:44:43 +0200

    fix: remove unneeded migrations

--- server/src/main/resources/db/migration/V15__exam_sessions_sentinel_pk.sql
+++ server/src/main/resources/db/migration/V15__exam_sessions_sentinel_pk.sql
@@ -1,4 +1,3 @@
 alter table fr_exam_sessions
     drop constraint fr_exam_sessions_pkey,
-    add primary key (sentinel_id),
-    add constraint fr_exam_sessions_student_id_unique unique (student_id);
+    add primary key (sentinel_id);
--- server/src/main/resources/db/migration/V16__exam_sessions_multi_session.sql
+++
@@ -1,2 +0,0 @@
-alter table fr_exam_sessions
-    drop constraint if exists fr_exam_sessions_student_id_unique;
