e09f95e0906876308c2c49848c99f8f0d385a421
EldinBegano <maskettwitch@gmail.com>
2026-05-25 13:18:06 +0200

    fix: remove unneeded migrations

--- server/src/main/resources/db/migration/V12__exam_sessions.sql
+++ server/src/main/resources/db/migration/V12__exam_sessions.sql
@@ -3,7 +3,7 @@ create table fr_exam_sessions (
     sentinel_id uuid not null,
     exam_id uuid not null,
     video_file_path varchar(1024) default null,
-    primary key (student_id),
+    primary key (student_id, exam_id),
     foreign key (student_id) references fr_student (id),
     foreign key (exam_id) references fr_exam (id)
 );
